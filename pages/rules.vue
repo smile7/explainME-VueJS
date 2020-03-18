07360fee6b634918e95ca785ba3524c974b05dc7
Smile <sisitymiteva@yahoo.com>
2020-03-18 12:43:53 +0200

    Rules page updated

--- pages/rules.vue
+++ pages/rules.vue
@@ -15,33 +15,40 @@
                 </div>
                 <div>
                   <div class="step">Стъпка II - Въведете думи</div>
-                  За всеки играч по 5 - 1 известна личност, 1 място и 3 думи. Ако предпочитате, можете да 
+                  За всеки играч по 5 - <strong>1 известна личност, 1 място и 3 думи</strong>. Ако предпочитате, можете да 
                   пропуснете тази стъпка и сложен алгоритъм автоматично ще избере случайни думи за вашата игра.
                 </div>
                 <div>
                   <div class="step">Цел</div>
-                  Всеки отбор има <span class="colorblue">30</span> секунди, за да познае възможно 
+                  Всеки отбор има <strong><span class="colorblue">30</span> секунди</strong>, за да познае възможно 
                   най-много думи. Печели този отбор, който събере най-много точки.
                 </div>
                 <div>
                   <div class="step">1-ви кръг - Oбяснение с думи</div>
                   Използвате колкото искате думи, за да обясните думата, която виждате. Не трябва да използвате нейн корен или 
-                  да казвате думата на друг език. За позната дума, печелите <span class="colorblue">3</span> точки.
+                  да казвате думата на друг език. <strong>За позната дума, печелите <span class="colorblue">3</span> точки.</strong>
                 </div>
                 <div>
                   <div class="step">2-ри кръг - Oбяснение с 1 дума</div>
-                  Използвате една единствена дума като обяснение. За позната дума, печелите <span class="colorblue">4</span> точки.
+                  Използвате една единствена дума като обяснение. <strong>За позната дума, печелите <span class="colorblue">4</span> точки.</strong>
                 </div>
                 <div>
                   <div class="step">3-ти кръг - Oбяснение с жестове</div>
-                  Използвате единствено жестове, всякакво говорене е забранено. За позната дума, печелите <span class="colorblue">5</span> точки.
+                  Използвате единствено жестове, всякакво говорене е забранено. <strong>За позната дума, печелите <span class="colorblue">5</span> точки.</strong>
                 </div>
                 <div>
                   <div class="step">Друго</div>
-                  <p>Ако някоя дума ви затруднява, можете да я пропуснете. За всяка пропусната дума, губите <span class="colorblue">1</span> точка.</p>
-                  <p>За да отбележите позната дума, натиснете <span class="colorblue">SPACE</span>.<br>
-                  За да пропуснете дума, натиснете <span class="colorblue">ESCAPE.</span></p>
-                  <h4 class="colorblue">ТОВА Е! ЗАБАВАЛЯВАЙТЕ СЕ :) </h4>
+                  <p>Ако някоя дума ви затруднява, можете да я пропуснете. <strong>За всяка пропусната дума, губите <span class="colorblue">1</span> точка.</strong></p>
+                  <strong>
+                    <p>За да отбележите позната дума, натиснете <span class="colorblue">SPACE</span>.<br>
+                    За да пропуснете дума, натиснете <span class="colorblue">ESCAPE.</span></p>
+                  </strong>
+                <div>
+                  <div class="step">Телефон</div>
+                  <p>Възможно е играта да се играе и на телефон, но най-удобно ще ви бъде през компютър. За да отбележите позната 
+                    дума на телефон, натиснете с пръст върху думата. За да пропуснете дума, плъзнете с цял пръст наляво или надясно отново върху нея. </p>
+                </div>
+                <h4 class="colorblue">ТОВА Е! ЗАБАВАЛЯВАЙТЕ СЕ :) </h4>
                 </div>
                 <img src="../static/images/joey.jpg">
                 <div class="iframe">
@@ -64,38 +71,6 @@ export default {
     Footer
   }
 }
-/*
-Methods / Buttons:
-V1.0:
- d- StartGame
- d- WordGuessed - btn AND space hit +1pt
- d- WordSkipped - btn AND esc hit -1pt
- - QuitGame - btn below right
-V1.1:
- d- Rounds
-
-View:
-V1.0:
- d- All Teams
- d- Points live changed for teams
- d- Timer
-V1.1:
- d- Add words
-V1.2:
- - Action log
-V3.0 Account:
- - Best score all time
-
-
-DB:
-V1:
- - Keep words
-V1.3:
- - Delete words
-V3.0 Account:
- - Keep best results - team names and scores
-*/
-
 </script>
 
 <style lang="scss" scoped>
